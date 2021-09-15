3a063bfb6008139510135e41d63f077e7a4d8ce2
Lars Eckart <lars.eckart@hey.com>
2021-09-15 22:14:33 +0300

    use compact record constructor

--- src/main/java/com/larseckart/math/Denominator.java
+++ src/main/java/com/larseckart/math/Denominator.java
@@ -2,15 +2,14 @@ package com.larseckart.math;
 
 record Denominator(int value){
 
-  Denominator(int value) {
+  Denominator {
     if (value == 0) {
       throw new IllegalArgumentException();
     }
-    this.value = value;
   }
 
   boolean isNegative() {
-    return this.value <0;
+    return this.value < 0;
   }
 
   Denominator revert() {
