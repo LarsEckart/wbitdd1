45a81db9630e0273fcedcac47d8de58f743a864d
Lars Eckart <lars.eckart@hey.com>
2021-10-15 13:49:06 +0300

    move output closer to the input

--- src/test/java/com/larseckart/pos/SellOneItemTest.java
+++ src/test/java/com/larseckart/pos/SellOneItemTest.java
@@ -3,6 +3,7 @@ package com.larseckart.pos;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.github.larseckart.tcr.FastTestCommitRevertMainExtension;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -13,13 +14,24 @@ import org.junit.jupiter.api.extension.ExtendWith;
 public class SellOneItemTest {
 
   @Test
-  void applesauce() {
+  void productFound() {
     Display display = new Display();
     Sale sale = new Sale();
 
     sale.onBarcode("12345");
 
     assertThat("$7.95").isEqualTo(display.getText());
+  }
+
+  @Test
+  @Disabled
+  void anotherProductFound() {
+    Display display = new Display();
+    Sale sale = new Sale();
+
+    sale.onBarcode("23456");
+
+    assertThat("$12.50").isEqualTo(display.getText());
   }
 
   static class Display {
