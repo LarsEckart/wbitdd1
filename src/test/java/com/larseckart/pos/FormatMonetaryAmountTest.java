223a566296e03755db9b2147c05bdcdbfb2b020e
Lars Eckart <lars.eckart@hey.com>
2021-10-20 11:08:13 +0300

    we can now format prices with trailing zeros

--- src/test/java/com/larseckart/pos/FormatMonetaryAmountTest.java
+++ src/test/java/com/larseckart/pos/FormatMonetaryAmountTest.java
@@ -2,18 +2,21 @@ package com.larseckart.pos;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.github.larseckart.tcr.FastTestCommitRevertMainExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+@ExtendWith(FastTestCommitRevertMainExtension.class)
 public class FormatMonetaryAmountTest {
 
   @ParameterizedTest(name = "{0} -> {1}")
   @CsvSource({"789, $7.89"})
-  void name(Integer priceInCents, String formattedPrice) {
+  void test(Integer priceInCents, String formattedPrice) {
     assertThat(formattedPrice).isEqualTo(format(priceInCents));
   }
 
   private String format(int priceInCents) {
-    return "$7.89";
+    return String.format("$%.2f", priceInCents / 100.0d);
   }
 }
