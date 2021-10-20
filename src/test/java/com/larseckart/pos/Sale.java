19a845129867269c09c673b982926b84c2fa97be
Lars Eckart <lars.eckart@hey.com>
2021-10-20 09:21:56 +0300

    push duplication out to the production code (tests)

--- src/test/java/com/larseckart/pos/Sale.java
+++ src/test/java/com/larseckart/pos/Sale.java
@@ -30,7 +30,8 @@ class Sale {
     if (price == null) {
       display.displayNoSaleInProgressMessage();
     } else {
-      display.text = "Total: $6.50";
+      display.text = "Total: "
+          + price;
     }
   }
 
